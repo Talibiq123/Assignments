5479eee1d0fbdd35325d29ce573e2e8f87d78276
Talibiq123 <talibsaeed90@gmail.com>
2023-03-20 22:05:36 +0530

    Initial commit

---
+++ src/flowofprogram/LeapYear.java
@@ -0,0 +1,10 @@
+package flowofprogram;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2000;
+        if (year % 4 == 0) System.out.println("Leap Year");
+        else
+            System.out.println("Not Leap Year");
+    }
+}
