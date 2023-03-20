629a59ca391c22ecc052b75d2ce0257692c8280b
Talibiq123 <talibsaeed90@gmail.com>
2023-03-20 22:33:21 +0530

    check after editing...

---
+++ src/firstjava/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package firstjava;
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enetr a Number : ");
+        int num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is an Even Number.");
+        } else {
+            System.out.println(num + " is an Odd Number.");
+        }
+    }
+}
