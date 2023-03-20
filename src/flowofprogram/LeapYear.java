9f43757c601f7c552326eee8a2341cd745d75729
Talibiq123 <talibsaeed90@gmail.com>
2023-03-20 22:20:29 +0530

    check after editing...

--- src/flowofprogram/LeapYear.java
+++ src/flowofprogram/LeapYear.java
@@ -2,7 +2,7 @@ package flowofprogram;
 
 public class LeapYear {
     public static void main(String[] args) {
-        int year = 2000; //take any year
+        int year = 2000; //take any year...
         if (year % 4 == 0) {
             System.out.println("Leap Year");
         }
