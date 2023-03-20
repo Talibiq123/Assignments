27e00f37ea6fb9d86fada9d57eff1b69055c0800
Talibiq123 <talibsaeed90@gmail.com>
2023-03-20 22:39:58 +0530

    comiting

--- src/firstjava/EvenOrOdd.java
+++ src/firstjava/EvenOrOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enetr a Number : ");
+        System.out.print("Enter a Number : ");
         int num = input.nextInt();
         if (num % 2 == 0) {
             System.out.println(num + " is an Even Number.");
