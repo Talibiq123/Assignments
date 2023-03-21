30a57cdbe3cb0aaad2d49ea03956db33dcd82b15
Talibiq123 <talibsaeed90@gmail.com>
2023-03-21 12:31:48 +0530

    commit

---
+++ src/firstjava/Palindrome.java
@@ -0,0 +1,28 @@
+// 8. To find out whether the given String is Palindrome or Not.
+package firstjava;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Palindrome");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        String str = sc.next();
+        int l = 0;
+        int r = str.length() - 1;
+        boolean ans = false;
+        while (l < r) {
+            if (str.charAt(l) == str.charAt(r)) {
+                ans = true;
+            }
+            l++;
+            r--;
+        }
+        if (ans == true) {
+            System.out.println(str +" is a Palindrome.");
+        } else {
+            System.out.println(str +" is not a Palindrome.");
+        }
+    }
+}
