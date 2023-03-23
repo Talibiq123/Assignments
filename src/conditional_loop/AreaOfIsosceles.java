28aaa5109f26d9ff1afea93d1a6848d61129998a
Talibiq123 <talibsaeed90@gmail.com>
2023-03-23 22:58:17 +0530

    commit

---
+++ src/conditional_loop/AreaOfIsosceles.java
@@ -0,0 +1,17 @@
+package conditional_loop;
+
+import java.util.Scanner;
+
+public class AreaOfIsosceles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the height of the Triangle = ");
+        double h = scanner.nextInt();
+        System.out.println("Enter the Base of the Triangle = ");
+        double b = scanner.nextInt();
+
+        double area = (h * b) / 2;
+
+        System.out.println("Area of the Isosceles = "+area);
+    }
+}
