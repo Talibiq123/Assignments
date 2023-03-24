6ab129b31d4a43724fa6b6ef82a5b4aa3e9c39a3
Talibiq123 <talibsaeed90@gmail.com>
2023-03-24 13:14:06 +0530

    commit

--- src/conditional_loop/VolumeOfPyramid.java
+++ src/conditional_loop/VolumeOfPyramid.java
@@ -1,4 +1,17 @@
 package conditional_loop;
 
+import java.util.Scanner;
+
 public class VolumeOfPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Length of Pyramid = ");
+        double l = scanner.nextDouble();
+        System.out.print("Enter the Base Width of Pyramid = ");
+        double w = scanner.nextDouble();
+        System.out.print("Enter the Height of Pyramid = ");
+        double h = scanner.nextDouble();
+        double volume = (l * w * h) / 3;
+        System.out.print("Volume of the Pyramid = "+volume);
+    }
 }
