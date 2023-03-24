f20001d96e0377b24231f35b9e131941d358f7a4
Talibiq123 <talibsaeed90@gmail.com>
2023-03-24 13:09:08 +0530

    commit

--- src/conditional_loop/VolumeOfSphere.java
+++ src/conditional_loop/VolumeOfSphere.java
@@ -1,4 +1,13 @@
 package conditional_loop;
 
+import java.util.Scanner;
+
 public class VolumeOfSphere {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the radius of Sphere = ");
+        double r = scanner.nextDouble();
+        double volume = (4 * Math.PI * r * r * r ) / 3;
+        System.out.print("Volume of Sphere = "+volume);
+    }
 }
