7dcfe9b95ede0ff685c2e0d1edc8a0575d04f947
Talibiq123 <talibsaeed90@gmail.com>
2023-03-24 12:43:34 +0530

    commit

--- src/conditional_loop/VolumeOfPrism.java
+++ src/conditional_loop/VolumeOfPrism.java
@@ -1,4 +1,18 @@
 package conditional_loop;
 
+import java.util.Scanner;
+
 public class VolumeOfPrism {
+    public static void main(String[] args) {
+        System.out.println("Only for Hexagonal Prism");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the Apothem Length of Prism = ");
+        int a = scanner.nextInt();
+        System.out.print("Enter the Base Length of Prism = ");
+        int b = scanner.nextInt();
+        System.out.print("Enter the Height Length of Prism = ");
+        int h = scanner.nextInt();
+        int volume = 3 * a * b * h;
+        System.out.print("Volume of Prism = "+volume);
+    }
 }
